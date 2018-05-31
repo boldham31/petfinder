eed905d76f17d9c1eb2c0b15c830f632828aa41d
Bryce Oldham <bryceoldham@gmail.com>
2018-05-31 15:39:04 -0500

    begins display of pet data into cards

--- petfinder-app/src/app/pet-card/pet-card.component.ts
+++ petfinder-app/src/app/pet-card/pet-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-pet-card',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pet-card.component.css']
 })
 export class PetCardComponent implements OnInit {
-
+  @Input() pet: any;
   constructor() { }
 
   ngOnInit() {
+    console.log(this.pet);
   }
 
 }
